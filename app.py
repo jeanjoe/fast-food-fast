b3dc99706ccd50aae0d3a2f8570a643ad76e0f86
jeanjoe <manzede@gmail.com>
2018-09-17 14:43:28 +0300

    [Delievrs #160545778] move routes to api file in models

--- app.py
+++ app.py
@@ -1,17 +1,5 @@
-from flask import Flask, jsonify
-from modules.orders import Order
+from models.api import app
 
-
-app = Flask(__name__)
-orders = Order()
-
-@app.route('/', methods=['GET'])
-def index():
-    return jsonify({'message': 'Fast-food-fast API endpoints. Navigate to api/v1/orders'}), 200
-
-@app.route('/api/v1/orders', methods=['GET'])
-def get_all_orders():
-    return jsonify({'orders': orders.get_all_orders()}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/api.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from models.orders import Order
+
+app = Flask(__name__)
+orders = Order()
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Fast-food-fast API endpoints. Navigate to api/v1/orders'}), 200
+
+@app.route('/api/v1/orders', methods=['GET'])
+def get_all_orders():
+    return jsonify({'orders': orders.get_all_orders()}), 200
