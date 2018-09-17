bb46bdc7e64b89f7c60c607dbf85fc0103592217
jeanjoe <manzede@gmail.com>
2018-09-17 16:01:28 +0300

    [Delivers #160545685] add create new order API endpoint

--- models/api.py
+++ models/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from models.orders import Order
 
 app = Flask(__name__)
@@ -11,3 +11,12 @@ def index():
 @app.route('/api/v1/orders', methods=['GET'])
 def get_all_orders():
     return jsonify({'orders': orders.get_all_orders()}), 200
+
+@app.route('/api/v1/orders', methods=['POST'])
+def add_order():
+    """Add new order to order lists."""
+    data = request.get_json()
+    saved_order = orders.add_order(data['menu_id'], data['client_id'], data['location'], data['quantity'])
+    if not saved_order:
+        return jsonify({"error": "Unable process your order"})
+    return jsonify({"data": saved_order}), 201
--- models/orders.py
+++ models/orders.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import uuid
 
 class Order:
 
@@ -7,4 +9,19 @@ class Order:
         self.orders = orders
 
     def get_all_orders(self):
+        """Get list of all orders."""
         return self.orders
+
+    def add_order(self, menu_id, client_id, location, quantity):
+        """Create New order."""
+        order = {
+            "id": str(uuid.uuid1()),
+            "menu_id": menu_id,
+            "client_id": client_id,
+            "location": location,
+            "quantity": quantity,
+            "status": "pending",
+            "created_at": str(datetime.now())
+        }
+        self.orders.append(order)
+        return order
