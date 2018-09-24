826e856d0ef217678903febab11243aaa1ee0a8d
Jean Joe Irascible <manzede@gmail.com>
2018-09-24 11:05:05 +0300

    [Fixes #160667136] change local url to heroku url

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -4,7 +4,7 @@ import requests
 from httmock import HTTMock
 from .mock import MockOrder
 
-BASE_URL = 'http://127.0.0.1:5000/api/v1/orders'
+BASE_URL = 'https://manzede-stackoverflow-lite.herokuapp.com/api/v1/orders'
 
 class OrderTest(unittest.TestCase):
     """Order Tests."""
