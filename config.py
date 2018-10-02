37b349069f18e1d934d9d3f70f30b4ddde64c51c
Jean Joe Irascible <manzede@gmail.com>
2018-10-02 12:45:51 +0300

    [Delivers #160918749] add config for testing and development

--- config.py
+++ config.py
@@ -1,9 +1,22 @@
-DEBUG = True
+class Config():
+    DEBUG = False
+    SECRET_KEY="whsdyuhgshji90woiryh3ikwgrdn9w7eui3kgwbnidjhnsmufycneh"
 
-DB_NAME= "fast_foods_fast"
-DB_USER= "postgres"
-DB_PASS= "manben"
-DB_HOST= "localhost"
-DB_PORT= "5433"
+class DevelopmentConfig(Config):
+    DEBUG = True
+    DB_NAME= "fast_foods_fast"
+    DB_USER= "postgres"
+    DB_PASS= "manben"
+    DB_HOST= "localhost"
+    DB_PORT= "5433"
 
-SECRET_KEY="whsdyuhgshji90woiryh3ikwgrdn9w7eui3kgwbnidjhnsmufycneh"
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+
+    DB_NAME= "fast_foods_fast"
+    DB_USER= "postgres"
+    DB_PASS= "manben"
+    DB_HOST= "localhost"
+    DB_PORT= "5433"
+    
