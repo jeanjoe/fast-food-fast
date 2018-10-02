c90dfe42b06539516e7d1dbc8ba986fc2c0c31f6
Jean Joe Irascible <manzede@gmail.com>
2018-10-02 16:24:50 +0300

    [Delivers #160923151] separate views

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,7 @@ from flask import Flask
 
 app = Flask("__name__")
 
-from app import view
+from app.views import main
+from app.views import user
+from app.views import admin
 from app.models import connection
