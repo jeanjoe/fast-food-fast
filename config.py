19be6df33c2b04f809c964ebd84262fa69980c4b
Jean Joe Irascible <manzede@gmail.com>
2018-10-20 22:36:27 +0300

    add heroku db credentials

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ class Config():
 
 
 class DevelopmentConfig(Config):
-    DEBUG = True
-    DB_NAME = "fast_foods_fast"
-    DB_USER = "postgres"
-    DB_PASS = "manben"
-    DB_HOST = "localhost"
-    DB_PORT = "5432"
+    DEBUG = False
+    DB_NAME= "d4554bh5hlg666"
+    DB_USER= "biizsdnusllfom"
+    DB_PASS= "a849805a16d39043891cb68791f88dee03bbd75fc78eb27873751749e7bf669e"
+    DB_HOST= "ec2-54-221-225-11.compute-1.amazonaws.com"
+    DB_PORT= "5432"
 
 
 class TestingConfig(Config):
@@ -25,4 +25,4 @@ class TestingConfig(Config):
     DB_USER = "postgres"
     DB_PASS = "manben"
     DB_HOST = "localhost"
-    DB_PORT = "5432"
+    DB_PORT = "5433"
