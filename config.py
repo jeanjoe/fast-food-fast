d2663a9e4d14d38d615ab239cc0c3a3943adabce
Jean Joe Irascible <manzede@gmail.com>
2018-10-25 15:04:34 +0300

    [Fixes #161408574] fix travis db connection

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ class Config():
 
 class DevelopmentConfig(Config):
     DEBUG = False
-    DB_NAME= "d4554bh5hlg666"
-    DB_USER= "biizsdnusllfom"
-    DB_PASS= "a849805a16d39043891cb68791f88dee03bbd75fc78eb27873751749e7bf669e"
-    DB_HOST= "ec2-54-221-225-11.compute-1.amazonaws.com"
-    DB_PORT= "5432"
+    DB_NAME = "fast_foods_fast"
+    DB_USER = "postgres"
+    DB_PASS = "manben"
+    DB_HOST = "localhost"
+    DB_PORT = "5432"
 
 
 class TestingConfig(Config):
